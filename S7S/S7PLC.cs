150531a7b3d43847493e4068775869a496894c58
metropolitan24 <141680173+metropolitan24@users.noreply.github.com>
2023-08-19 06:48:00 +0100

    Update S7PLC.cs

--- S7S/S7PLC.cs
+++ S7S/S7PLC.cs
@@ -1,4 +1,4 @@
-﻿using Sharp7;
+using Sharp7;
 
 namespace Sharp7Simplified
 {
@@ -6,6 +6,7 @@ namespace Sharp7Simplified
     {
         private bool _connectionSucceeded;
         private S7Client _client;
+        private int k;
 
         public bool ConnectionState
         {
@@ -23,7 +24,77 @@ namespace Sharp7Simplified
             else
             {
                 _connectionSucceeded = false; Console.WriteLine(_client.ErrorText(connectionResult));
+            }
+        }
 
+        public int Read(int db, int nodepos, out object tag) 
+        {
+            try
+            {
+                byte[] bufferDB = new byte[18];
+                int readResult = _client.DBRead(db, 0, 18, bufferDB);
+                if (readResult != 0)
+                {
+                    Console.WriteLine(_client.LastError);
+                }
+                tag = 0;
+                switch (Type.GetTypeCode(tag.GetType()))
+                {
+                    case TypeCode.Boolean:
+                        tag = S7.GetBitAt(bufferDB, nodepos, 0);
+                        break;
+                    case TypeCode.Byte:
+                        tag = S7.GetByteAt(bufferDB, nodepos);
+                        break;
+                    case TypeCode.Char:
+                        tag = S7.GetCharsAt(bufferDB, nodepos, 1);
+                        break;
+                    case TypeCode.Int32:
+                        tag = S7.GetIntAt(bufferDB, nodepos);
+                        break;
+                    case TypeCode.UInt32:
+                        tag = S7.GetUIntAt(bufferDB, nodepos);
+                        break;
+                }
+
+                return 1;
+            }
+            catch(Exception e)
+            {
+                tag = 0;
+                return 0;
+            }
+        }
+
+        public int Write(int db, int nodepos, object writeVar)
+        {
+            try
+            {
+                byte[] writeBuffer = new byte[12];
+                switch (Type.GetTypeCode(writeVar.GetType()))
+                {
+                    case TypeCode.Boolean:
+                        S7.SetBitAt(writeBuffer, nodepos, 0, (bool)writeVar);
+                        break;
+                    case TypeCode.Byte:
+                        S7.SetByteAt(writeBuffer, nodepos, 0);
+                        break;
+                    case TypeCode.Char:
+                        S7.SetCharsAt(writeBuffer, nodepos, writeVar.ToString());
+                        break;
+                    case TypeCode.Int32:
+                        S7.SetIntAt(writeBuffer, nodepos, (short)writeVar);
+                        break;
+                    case TypeCode.UInt32:
+                        S7.SetUIntAt(writeBuffer, nodepos, (ushort)writeVar);
+                        break;
+                }
+
+                return 1;
+            }
+            catch (Exception e)
+            {
+                return 0;
             }
         }
     }
